d155506e47c2f994d1ddd1a2be9be12a66f44c7a
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-02-03 14:10:42 +0200

    Static Expenses components have been added

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.items.map((item) => {
+        return (
+          <ExpenseItem
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        );
+      })}
+    </Card>
+  );
+}
+
+export default Expenses;
