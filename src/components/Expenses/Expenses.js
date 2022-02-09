3bee68abd12fc062783d6587a40e82bab12bfbb5
Anastasiia Lialina <lialina.anastasiia@gmail.com>
2022-02-09 18:12:04 +0200

    ExpensesFilter component with responsive filtering has been added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,21 +1,43 @@
-import React from "react";
-import ExpenseItem from "./ExpenseItem";
+import React, { useState } from "react";
+
+import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 
 function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("");
+  const [visibleExpenses, setVisibleExpenses] = useState(props.items);
+
+  const filterYearHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear);
+    setVisibleExpenses(
+      props.items.filter(
+        (expense) => String(expense.date.getFullYear()) === selectedYear
+      )
+    );
+  };
+
   return (
-    <Card className="expenses">
-      {props.items.map((item) => {
-        return (
-          <ExpenseItem
-            title={item.title}
-            amount={item.amount}
-            date={item.date}
-          />
-        );
-      })}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onSelectYear={filterYearHandler}
+        />
+        {visibleExpenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          );
+        })}
+      </Card>
+    </div>
   );
 }
 
